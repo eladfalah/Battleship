14437b0f2d815e0418e07457669c04225da8f161
eladfalah <eladfala2@gmail.com>
2023-12-29 23:37:22 +0200

    closing the grid

--- src/ui/buildgrid.h
+++ src/ui/buildgrid.h
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "setting.h"
+#include <string.h>
 void moveCursor(int x, int y) {
     printf("\e[%d;%dH", y, x);
 }
-void printLineSection(int i, int j, char a, char b, char c){
-
-			moveCursor(j, i);
-			printf("%c%c%c",a, b, c);
-			
+void printLineSection(int x, int y, const char *chars){
+			int i = 0;
+			char printedChar;
+			moveCursor( y, x);
+			printedChar=chars[i];
+			while (printedChar!='E'){
+				printf("%c",printedChar);
+				i++;
+				printedChar=chars[i];
+			}
 }
 void buildgrid(){
-	for(int i=0;i<28;i+=2){
-		for(int j=0;j<150;j+=3){
-		printLineSection(i, j,'+','-','-');
-		printLineSection(i+1, j, '|', ' ', ' ');
+	int i, j;
+	for(i=0;i<=28;i+=2){
+		for(j=0;j<=150;j+=3){
+		printLineSection(i, j, (j < 150) ? "+--E" : "+E");
+		if(i<28)printLineSection(i+1, j, (j < 150) ? "|  E" : "|E");
 		}
 		
+		//printLineSection(i, j,'+',' ',' ');
+		//printLineSection(i+1, j, '|', ' ', ' ');
 		
+//	}
+	//for(j=0;j<28;j++){
+	//	printLineSection(i+1, j,'+','-','-');
 		
-	}
+	}//
 fflush(stdout);
 }
