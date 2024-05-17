64f1db897ad13b11d9a71600af57abf7477cb5b0
eladfalah <eladfala2@gmail.com>
2024-05-17 15:10:43 +0300

    changing notes

--- src/backend/main.asm
+++ src/backend/main.asm
@@ -4,7 +4,7 @@ section .data;
 inp_ship db "a","b","c"; user input ship from the ui ()
 section .bss;
 ships resb 80;ships array
-;inp_ship resb 3; x pos (8 bits), y pos (8 bits), size (2 bits), isSinked (1 bit), hits points (5 bites); 
+;inp_ship resb 3; x pos (8 bits), y pos (8 bits), size (2 bits), isHorizontal (1 bit), hits points (5 bites); 
 section .text;
 	global ROWS;
 	global COLUMNS;
